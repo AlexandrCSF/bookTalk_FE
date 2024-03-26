bb0bcf00d64d4359f8aa18f9f7f64da61fbbd733
zhekkkk <fedo5eeW@yandex.ru>
2024-03-26 17:29:42 +0300

    fix: change date formatting for calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 import 'booktalk_app.dart';
 
 void main() {
-  runApp(
+  initializeDateFormatting().then((_) => runApp(
     DevicePreview(
       enabled: kIsWeb,
       builder: (context) => BookTalkApp(),
     )
-  );
+  ));
 }
