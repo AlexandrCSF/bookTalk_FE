f1d53be7f7d63720b541d9a4bd066b4831d0f2f4
marylarina <mashalar1510@gmail.com>
2024-04-02 23:49:57 +0300

    feat: add widget for text fields

---
+++ lib/pages/widgets/textfield_widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TextFieldWidget extends StatefulWidget {
+  final String label;
+  final String hintText;
+  final String text;
+
+
+  const TextFieldWidget({super.key, required this.label, required this.hintText, required this.text});
+
+  @override
+  State<TextFieldWidget> createState() => _TextFieldWidgetState();
+}
+
+class _TextFieldWidgetState extends State<TextFieldWidget> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    final text = Theme.of(context).textTheme;
+    return Padding(
+      padding: const EdgeInsets.only(top: 10, bottom: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Text(
+              widget.label,
+              style: text.headlineLarge
+                  ?.copyWith(color: colors.onBackground),
+            ),
+          ),
+          TextFormField(
+            /*controller: controller,*/
+            initialValue: widget.text,
+            style: text.labelMedium
+                ?.copyWith(color: colors.onBackground),
+            decoration: InputDecoration(
+              hintText: widget.hintText,
+              hintStyle:
+              text.labelMedium?.copyWith(color: colors.outline),
+              contentPadding:
+              const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              fillColor: colors.onInverseSurface,
+              filled: true,
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  width: 2,
+                  color: colors.inversePrimary,
+                ),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  width: 2,
+                  color: colors.inversePrimary,
+                ),
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
