f6155a39f6ac3d23729299b4fa076c5bf7e2112b
zhekkkk <fedo5eeW@yandex.ru>
2024-04-02 18:38:17 +0300

    feat: implement discussion page

---
+++ lib/pages/discussion_page/widgets/comment_card.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class CommentCard extends StatelessWidget {
+
+  const CommentCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme
+        .of(context)
+        .colorScheme;
+    final text = Theme
+        .of(context)
+        .textTheme;
+    return Card(
+        surfaceTintColor: colors.surface,
+        elevation: 3,
+        shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(16)),
+          side: BorderSide(
+            color: colors.primary,
+            width: 1,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 15,
+                    backgroundImage: Image
+                        .asset('lib/images/5_3.jpg')
+                        .image,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      'Имя Фамилия',
+                      style: text.headlineMedium
+                          ?.copyWith(color: colors.onSurface),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  'Также как базовый вектор развития способствует подготовке и '
+                      'реализации системы массового участия. Ясность нашей позиции '
+                      'очевидна: консультация с широким активом позволяет оценить '
+                      'значение благоприятных перспектив. В частности, глубокий '
+                      'уровень погружения, а также свежий взгляд на привычные '
+                      'вещи — безусловно открывает новые горизонты для позиций, '
+                      'занимаемых участниками в отношении поставленных задач.',
+                  style: text.bodyMedium?.copyWith(color: colors.outline),
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
--- lib/pages/event_list_page/event_list_page.dart
+++ lib/pages/event_list_page/event_list_page.dart
@@ -7,8 +7,49 @@ class EventListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("event list page"),
+    final colors = Theme.of(context).colorScheme;
+    final text = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: colors.background,
+        elevation: 0,
+        surfaceTintColor: colors.background,
+        leading: AutoLeadingButton(
+          color: colors.primary,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10.0),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                MdiIcons.plus,
+                color: colors.primary,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+        child: ListView.builder(
+          itemCount: 7,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 20.0, top: 20.0),
+                child: Text(
+                  "Обсуждения",
+                  style: text.headlineLarge?.copyWith(color: colors.onSurface),
+                ),
+              );
+            }
+            else {
+              return EventCard();
+            }
+          },
+        ),
+      ),
     );
   }
 }
