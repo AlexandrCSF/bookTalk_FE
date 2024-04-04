06cacde9529002fae11e1d2560de54b342c53ea7
marylarina <mashalar1510@gmail.com>
2024-04-04 21:45:17 +0300

    feat: implement authorization page

--- lib/pages/authorization_page/authorization_page.dart
+++ lib/pages/authorization_page/authorization_page.dart
@@ -1,5 +1,10 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:booktalk_frontend/navigation/app_router.dart';
+import 'package:booktalk_frontend/pages/widgets/textfield_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+import '../widgets/main_primary_button.dart';
 
 @RoutePage()
 class AuthorizationPage extends StatelessWidget {
@@ -7,8 +12,63 @@ class AuthorizationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('authorization'),
+    final colors = Theme.of(context).colorScheme;
+    final text = Theme.of(context).textTheme;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 60, right: 60, top: 115),
+          child: Align(
+            alignment: Alignment.center,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 61,
+                  child: Image.asset(
+                    'lib/images/logo.png',
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 46),
+                  child: Text(
+                    "С возвращением!",
+                    style:
+                        text.titleMedium?.copyWith(color: colors.onBackground),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 55),
+                  child: TextFieldWidget(
+                      label: "Логин",
+                      hintText: "Введите логин",
+                      text: "",
+                      maxLines: 1),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: TextFieldWidget(
+                      label: "Пароль",
+                      hintText: "Введите пароль",
+                      text: "",
+                      maxLines: 1),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(left: 60, right: 60, bottom: 60),
+        child: MainPrimaryButton(
+          label: "Войти",
+          icon: MdiIcons.login,
+          onTap: (){
+            context.router.navigate(const MyEventsRoute());
+          },
+        ),
+      ),
     );
   }
 }
